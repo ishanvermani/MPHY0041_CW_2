6c1d1993655cf2fe3ba0d5b4e7e0e4549ae4190c
Charvi Agarwal <charviagarwal2002@gmail.com>
2025-12-23 18:05:17 -0600

    patch sampling

--- cw2-pt/dataset.py
+++ cw2-pt/dataset.py
@@ -3,7 +3,7 @@ import nibabel as nib
 import numpy as np
 import torch
 from torch.utils.data import Dataset
-
+import torch.nn.functional as F
 from preprocess import image_file_to_mask_file
 
 class MalePelvicDataset(Dataset):
@@ -13,7 +13,7 @@ class MalePelvicDataset(Dataset):
         self.mask_dir = self.data_dir / "masks"
 
         self.image_files = sorted(
-            [p for p in self.image_dir.iterdir() if p.name.endswith(".nii") or p.endswith(".nii.gz")]
+            [p for p in self.image_dir.iterdir() if p.name.endswith(".nii") or p.name.endswith(".nii.gz")]
         )
 
         # No need to normalize because done already in preprocess.py
@@ -42,3 +42,47 @@ class MalePelvicDataset(Dataset):
 
         return image, mask
 
+class PatchDataset(Dataset):
+    def __init__(self, vol_dataset: Dataset, patch_size =(16, 192, 192), foreground_prob=0.8,samples_per_volume=8):
+
+        self.dataset = vol_dataset
+        self.pz, self.py, self.px = patch_size
+        self.foreground_prob = foreground_prob
+        self.samples_per_volume = samples_per_volume
+
+    def __len__(self):
+        return len(self.dataset) * self.samples_per_volume
+
+    def __pad__(self, image, mask):
+        _, z, y, x = image.shape
+        pad_z = max(0, self.pz - z)
+        pad_y = max(0, self.py - y)
+        pad_x = max(0, self.px - x)
+        
+        if pad_z or pad_y or pad_x:
+            image = F.pad(image, (0, pad_x, 0, pad_y, 0, pad_z), value=0.0)
+            mask = F.pad(mask, (0, pad_x, 0, pad_y, 0, pad_z), value=0)
+        return image, mask
+    
+    def __getitem__(self, idx):
+        vol_idx = idx // self.samples_per_volume
+        image, mask = self.dataset[vol_idx]
+
+        image, mask = self.__pad__(image, mask)
+        _, z, y, x = image.shape
+
+        if torch.rand(1).item() < self.foreground_prob and (mask > 0).any():
+            fg = torch.nonzero(mask > 0, as_tuple=False)
+            zc, yc, xc = fg[torch.randint(0, fg.shape[0], (1,)).item()].tolist()
+            z0 = max(0, min(zc - self.pz // 2, z - self.pz))
+            y0 = max(0, min(yc - self.py // 2, y - self.py))
+            x0 = max(0, min(xc - self.px // 2, x - self.px))
+        else:
+            z0 = torch.randint(0, z - self.pz + 1, (1,)).item()
+            y0 = torch.randint(0, y - self.py + 1, (1,)).item()
+            x0 = torch.randint(0, x - self.px + 1, (1,)).item()
+
+        image_patch = image[:, z0:z0+self.pz, y0:y0+self.py, x0:x0+self.px]
+        mask_patch = mask[z0:z0+self.pz, y0:y0+self.py, x0:x0+self.px]
+
+        return image_patch, mask_patch
