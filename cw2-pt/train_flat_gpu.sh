ab91f0e563295ea27ddba7e59de97262f644d91c
rmapcag <rmapcag@login13.myriad.ucl.ac.uk>
2026-01-11 15:42:40 +0000

    small renamining fix

--- cw2-pt/train_flat_gpu.sh
+++ cw2-pt/train_flat_gpu.sh
@@ -4,7 +4,7 @@
 #$ -N flat_unet_10ep
 #$ -o logs/$JOB_NAME.$JOB_ID.out
 #$ -e logs/$JOB_NAME.$JOB_ID.err
-#$ -l h_rt=02:00:00
+#$ -l h_rt=07:00:00
 #$ -l mem=16G
 #$ -pe smp 4
 #$ -l gpu=1
@@ -17,11 +17,11 @@ PY=/myriadfs/home/rmapcag/y/envs/cw2-pt/bin/python
 
 $PY train.py \
   --data_dir data/preprocessed_data \
-  --epochs 10 \
+  --epochs 50 \
   --batch_size 1 \
   --lr 1e-3 \
   --num_classes 9 \
   --samples_per_volume 8 \
   --foreground_prob 0.8 \
   --num_workers 4 \
-  --metrics_path data/metrics
+  --metrics_path metrics/
