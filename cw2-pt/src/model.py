4218bdd3bb4f04d594373fa3b1c7687262b8c854
rmapcag <rmapcag@login13.myriad.ucl.ac.uk>
2026-01-11 15:39:55 +0000

    training_flat_with_gpu

--- cw2-pt/src/model.py
+++ cw2-pt/src/model.py
@@ -10,7 +10,7 @@ class ConvBlock(nn.Module):
         self.conv = nn.Sequential(
             nn.Conv2d(in_channels, out_channels, kernel_size=3, padding = 1),
             nn.ReLU(inplace=True),
-            nn.Conv2d(in_channels, out_channels, kernel_size=3, padding = 1),
+            nn.Conv2d(out_channels, out_channels, kernel_size=3, padding = 1),
             nn.ReLU(inplace=True)
         )
 
