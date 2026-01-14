9882bf8b103e3fa32c3cd07ac58cbf580afc7631
rmapcag <rmapcag@login13.myriad.ucl.ac.uk>
2026-01-14 07:45:30 +0000

    super dice flat training

--- cw2-pt/train.py
+++ cw2-pt/train.py
@@ -149,7 +149,7 @@ def train_one_epoch(model, loader, optimizer, device, loss_fn, num_classes, D=No
 	#, avg_super_dice, avg_super_auc, per_class_dice_vals, per_class_hd95_vals, conf_H
 
 
-def evaluate(model, loader, device, loss_fn, num_classes, D=None):
+def evaluate(model, loader, device, loss_fn, num_classes, D=None, alpha=None):
 	model.eval()
 	total_loss, total_px, total_dice, dice_count = 0.0, 0, 0.0, 0
 	total_hcost, hcost_count = 0.0, 0 
@@ -163,7 +163,7 @@ def evaluate(model, loader, device, loss_fn, num_classes, D=None):
 			images, masks = images.to(device), masks.to(device)
 			images, masks = reshape_to_2d(images, masks)
 			logits = model(images)
-			loss = loss_fn(logits, masks)
+			loss = loss_fn(logits, masks, alpha)
 
 			total_loss += loss.item() * masks.numel()
 			preds = logits.argmax(dim=1)
@@ -296,10 +296,10 @@ def main():
 			print(f"Training alpha {a} for {args.training_epochs} epochs")
 			for epoch in range(args.training_epochs):
 			
-				train_loss, train_dice = train_one_epoch(
+				train_loss, train_dice, train_super_dice, train_super_auc = train_one_epoch(
 					model, train_loader, optimizer, device, loss_fn, args.num_classes, D if args.use_hierarchical_loss else None, a if args.use_hierarchical_loss else None)
 			
-			val_loss, val_dice, val_hcost = evaluate(
+			val_loss, val_dice, val_hcost, val_super_dice, val_super_auc = evaluate(
 			 	model, val_loader, device, loss_fn, args.num_classes, D if args.use_hierarchical_loss else None, a if args.use_hierarchical_loss else None)
 			dt = time.time() - t0
 
