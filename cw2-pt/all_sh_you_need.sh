fd3cafe27d35f3982b55a75986f794af4d013179
rmapcag <rmapcag@login12.myriad.ucl.ac.uk>
2026-01-14 21:47:31 +0000

    rough instructions file

--- cw2-pt/all_sh_you_need.sh
+++ cw2-pt/all_sh_you_need.sh
@@ -117,3 +117,34 @@ $PY train.py \
 	--foreground_prob 0.8 \
 	--num_workers 4 \
 	--metrics_path metrics/
+
+#!/bin/bash
+#############################################
+# Test/evaluate flat vs hierarchical U-Net on GPU (SGE job script)
+#############################################
+#$ -S /bin/bash
+#$ -cwd
+#$ -N test
+#$ -o logs/$JOB_NAME.$JOB_ID.out
+#$ -e logs/$JOB_NAME.$JOB_ID.err
+#$ -l h_rt=02:00:00
+#$ -l mem=16G
+#$ -pe smp 4
+#$ -l gpu=1
+#$ -M rmapcag@ucl.ac.uk
+#$ -m abe
+
+set -euo pipefail
+mkdir -p logs
+
+PY=/myriadfs/home/rmapcag/y/envs/cw2-pt/bin/python
+
+$PY test.py \
+  --data_dir data/preprocessed_data/test \
+  --flat_ckpt model_weights/best_model_flat.pt \
+  --hier_ckpt model_weights/best_model_heir.pt \
+  --num_classes 9 \
+  --use_hierarchical_metrics \
+  --save_h_conf metrics/h_conf_{model}.npy
+  --out_csv metrics/test_metrics.csv \
+  --out_json metrics/test_metrics.json
